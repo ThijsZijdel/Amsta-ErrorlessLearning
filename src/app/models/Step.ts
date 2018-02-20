9e995fbfcaf34e9ced46d0eac5c691b9b199a7a5
Thijs <thijs.zijdel@hva.nl>
2018-02-20 22:31:50 +0100

    Constructor example

--- src/app/models/Step.ts
+++ src/app/models/Step.ts
@@ -2,4 +2,10 @@ export class Step {
   id: number;
   stepImgLink: string;
   stepDescription: string;
+
+  constructor(id: number, stepImgLink: string, stepDescription: string) {
+    this.id = id;
+    this.stepImgLink = stepImgLink;
+    this.stepDescription = stepDescription;
+  }
 }
