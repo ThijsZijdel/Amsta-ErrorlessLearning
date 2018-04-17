996c063663fc9c7b39f9891d60b9c3f1179f37ae
Thijs <thijs.zijdel@hva.nl>
2018-04-17 18:48:29 +0200

    V2 - fixed validating and displaying available tasks
    TODO: minute check

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,28 +15,73 @@ export class DashboardComponent implements OnInit {
 
   currentTasks: Task[] = [];
 
+  currentHour: number;
+
   constructor(private taskService: TaskService) { }
 
   /**
    * On initialize get those tasks.
    */
   ngOnInit() {
-    this.getTasks();
-    this.currentTime = "10:30";
+    this.getCurrentTasks();
 
-    this.filterTasks();
+    var now = new Date();
+    this.currentTime = now.getHours()+":"+now.getMinutes();
+
+    this.currentHour = parseInt(this.currentTime.substring(0, this.currentTime.indexOf(":")));
+   // this.filterTasks();
   }
 
   /**
    * Get all the tasks
    * @author Thijs Zijdel
    */
-  getTasks(): void {
-    this.taskService.getTasks()
-      .subscribe(tasks => this.tasks = tasks);
+  getCurrentTasks(): void {
+
+
+    this.taskService.getTasks().subscribe(tasks => {
+
+
+
+      for(let taskCheck of tasks){
+
+
+
+
+
+        for(let time of taskCheck.taskTimes){
+
+
+
+          let startHour = parseInt(time.startTime.substring(0, time.startTime.indexOf(":")));
+          let startMin = parseInt(time.startTime.substring(time.startTime.indexOf(":"),time.startTime.length));
+
+          let endHour = parseInt(time.endTime.substring(0, time.endTime.indexOf(":")));
+          let endMin = parseInt(time.endTime.substring(time.endTime.indexOf(":"),time.endTime.length));
+
+          console.log("test:"+startHour+" from "+time.startTime+" till: "+endHour + "  =_>"+time.endTime);
+
+
+
+          if (this.currentHour >= startHour && this.currentHour <= endHour) {
+            this.tasks.push(taskCheck);
+          }
+        }
+      }
+
+
+
+
+
+        //this.tasks = tasks as Task[]
+
+      });
   }
 
   filterTasks(): void {
+
+      window.alert(this.tasks.length);
+
       for(let taskCheck of this.tasks){
         for(let time of taskCheck.taskTimes){
           let startHour = time.startTime.substring(0,time.startTime.indexOf(":"));
