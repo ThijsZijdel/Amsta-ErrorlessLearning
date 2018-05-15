59a650fe778f0aa52dec117850b52f596858691c
Thijs <thijs.zijdel@hva.nl>
2018-05-15 16:01:39 +0200

    Activity sorting + display

--- src/app/admin/resident/resident.component.ts
+++ src/app/admin/resident/resident.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Resident} from "../../models/Resident";
+import {ResidentService} from "../../services/resident.service";
+import {ActivatedRoute} from "@angular/router";
+import {Activity} from "../../models/Activity";
 
 @Component({
   selector: 'app-resident',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResidentComponent implements OnInit {
 
-  constructor() { }
+  resident: Resident;
+  doneTasks: Activity[];
+  todoTasks: Activity[];
+  todayTasks: Activity[];
+
+  today: Date = new Date();
+
+  constructor(private route: ActivatedRoute,
+              private residentService: ResidentService) { }
 
   ngOnInit() {
+    this.getResident();
+    this.orderActivities();
   }
 
+  getResident(){
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.residentService.getResident(id)
+      .subscribe(resident => this.resident = resident);
+  }
+
+  private orderActivities() {
+    for(let activity of this.resident.activities){
+
+      if (activity.date === this.today)
+        this.todayTasks.push(activity)
+      else if (activity.completed)
+        this.doneTasks.push(activity)
+      else
+        this.todoTasks.push(activity)
+
+    }
+  }
 }
